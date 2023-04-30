347bd219a421e20ac6cc453fac11455511388003
Jiraphat <jiraphat4352@gmail.com>
2023-04-30 13:22:27 +0700

    javasript update

---
+++ Script4.js
@@ -0,0 +1,20 @@
+console.log(5+1);
+let a=20
+let b=10
+let c=10
+let d=10
+
+console.log(a+b) 
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a/b) 
+console.log(a%b) //ผลเศษ
+console.log(a**b) //ยกกำลัง
+
+console.log("ค่าเท่ากันหรือไม่ =",c==b)
+console.log("ค่าไม่เท่ากันหรือไม่ =",c!=b)
+console.log("ค่าcมากกว่าbหรือไม่ =",c>b)
+console.log("ค่าcน้อยกว่าbหรือไม่ =",c<b)
+console.log("ค่าcมากกว่าหรือเท่ากับbหรือไม่ =",c>=b)
+console.log("ค่าcน้อยกว่าหรือเท่ากับbหรือไม่ =",c<=b)
