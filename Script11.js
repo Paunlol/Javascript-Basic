dca15cebe657770aff19128826c0dfa3c972f327
Jiraphat <jiraphat4352@gmail.com>
2023-05-15 12:30:27 +0700

    ศึกษาJava Script

---
+++ Script11.js
@@ -0,0 +1,16 @@
+// if..else แบบลดรูป (Ternary Operator)
+
+let score = 51;
+let pass;
+
+// if(score>50){
+//     pass = "ผ่านเกณฑ์"
+// }
+// else{
+//     pass = "ไม่ผ่านเกณฑ์"
+// }
+// console.log(pass)
+
+//แบบลดรูป
+pass = score>50 ? "ผ่านเกณฑ์" : "ไม่ผ่านเกณฑ์"
+console.log(pass)
